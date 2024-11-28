2328dc8ca225869438c6885c34bc74153832e983
Carlsson111 <Linusjaa1@gmail.com>
2024-11-28 08:56:59 +0100

    Not fylly implemented yet

--- src/main/java/se/lexicon/Exercises.java
+++ src/main/java/se/lexicon/Exercises.java
@@ -1,6 +1,11 @@
 package se.lexicon;
 
 import se.lexicon.data.DataStorage;
+import se.lexicon.model.Gender;
+import se.lexicon.model.Person;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class Exercises {
 
@@ -11,6 +16,8 @@ public class Exercises {
     */
     public static void exercise1(String message) {
         System.out.println(message);
+        List<Person> eriks = storage.findMany(person -> person.getFirstName().equalsIgnoreCase("Erik"));
+        System.out.println(eriks);
         //Write your code here
 
         System.out.println("----------------------");
@@ -21,7 +28,8 @@ public class Exercises {
      */
     public static void exercise2(String message) {
         System.out.println(message);
-        //Write your code here
+        List<Person> allFemales = storage.findMany(person -> person.getGender()== Gender.FEMALE);
+        System.out.println(allFemales);
 
         System.out.println("----------------------");
     }
@@ -31,6 +39,8 @@ public class Exercises {
      */
     public static void exercise3(String message) {
         System.out.println(message);
+        List<Person> bornAfter2000 = storage.findMany(person -> person.getBirthDate().isAfter(LocalDate.of(2000,1,1)));
+        System.out.println(bornAfter2000);
         //Write your code here
 
         System.out.println("----------------------");
@@ -41,7 +51,8 @@ public class Exercises {
      */
     public static void exercise4(String message) {
         System.out.println(message);
-        //Write your code here
+        Person person = storage.findOne(p ->p.getId()==123);
+        System.out.println(person);
 
         System.out.println("----------------------");
 
@@ -53,6 +64,12 @@ public class Exercises {
      */
     public static void exercise5(String message) {
         System.out.println(message);
+
+        String personStr = storage.findOneAndMapToString(
+                p -> p.getId()==456,
+                p -> "Name: " + p.getFirstName() + " " + p.getLastName() + " born " +p.getBirthDate()
+        );
+        System.out.println(personStr);
         //Write your code here
 
         System.out.println("----------------------");
@@ -63,7 +80,14 @@ public class Exercises {
      */
     public static void exercise6(String message) {
         System.out.println(message);
-        //Write your code here
+        List<String> malesWithE = storage.findManyAndMapEachToString(
+                p -> p.getGender()==Gender.MALE && p.getFirstName().startsWith("E"),
+                p -> "Name: " + p.getFirstName() + " " + p.getLastName() + " born " +p.getBirthDate()
+        );
+        malesWithE.forEach(System.out::println);
+
+
+
 
         System.out.println("----------------------");
     }
@@ -74,7 +98,7 @@ public class Exercises {
      */
     public static void exercise7(String message) {
         System.out.println(message);
-        //Write your code here
+
 
         System.out.println("----------------------");
     }
@@ -84,7 +108,12 @@ public class Exercises {
      */
     public static void exercise8(String message) {
         System.out.println(message);
-        //Write your code here
+        storage.findAndDo(
+                p -> p.getFirstName().equalsIgnoreCase("Ulf"),
+                p -> System.out.println(p)
+
+        );
+
 
         System.out.println("----------------------");
     }
@@ -94,7 +123,10 @@ public class Exercises {
      */
     public static void exercise9(String message) {
         System.out.println(message);
-        //Write your code here
+        storage.findAndDo(
+                person -> person.getLastName().contains(person.getFirstName()),
+                person -> System.out.println(person)
+        );
 
         System.out.println("----------------------");
     }
@@ -104,7 +136,8 @@ public class Exercises {
      */
     public static void exercise10(String message) {
         System.out.println(message);
-        //Write your code here
+
+
 
         System.out.println("----------------------");
     }
